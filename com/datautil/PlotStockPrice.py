60d0b3138d2d2d7dd7c63c80677128831fad73e3
zhenxinlei <zhenxinlei@gmail.com>
2014-11-09 13:54:31 -0500

    get daily data, todo: get symbols from excel

--- com/datautil/PlotStockPrice.py
+++ com/datautil/PlotStockPrice.py
@@ -11,7 +11,7 @@ class PlotStockPrice:
         stockData = dict()
 
     def plotStockPrice(self):
-        #stockData= StockMinData.StockMinData().stockData
+        stockData= StockMinData.StockMinData().stockData
         print(stockData)
 
 PlotStockPrice().plotStockPrice()
